8582f21e785ff6c07967783f3b84749c83708c31
Vitaliy <kutukov@list.ru>
2022-04-15 14:48:21 +0300

    next commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 #include <QMediaPlayer>
 #include <QAudioOutput>
-//#include <QMediaContent>
+#include <QVideoWidget>
 
 #include <QVBoxLayout>
 #include <QHBoxLayout>
@@ -19,18 +19,20 @@ int main(int argc, char *argv[])
     QWidget playerWindow;
     QVBoxLayout vbox(&playerWindow);
     QHBoxLayout buttons;
+    auto *videoWidget = new QVideoWidget(&playerWindow);
     QSlider *songSlider = new QSlider (&playerWindow);
     songSlider->setOrientation(Qt::Horizontal);
+    vbox.addWidget(videoWidget);
     vbox.addWidget(songSlider);
 
     auto *openFileButton = new QPushButton("Open", &playerWindow);
     auto *playMediaButton = new QPushButton("Play", &playerWindow);
-    auto *pauseMeiaButton = new QPushButton("Pause", &playerWindow);
+    auto *pauseMediaButton = new QPushButton("Pause", &playerWindow);
     auto *stopMediaButton = new QPushButton("Stop", &playerWindow);
 
     buttons.addWidget(openFileButton);
     buttons.addWidget(playMediaButton);
-    buttons.addWidget(pauseMeiaButton);
+    buttons.addWidget(pauseMediaButton);
     buttons.addWidget(stopMediaButton);
 
     vbox.addLayout(&buttons);
@@ -39,33 +41,50 @@ int main(int argc, char *argv[])
     auto *audioOutput = new QAudioOutput;
     auto *player = new QMediaPlayer(&playerWindow);
     player->setAudioOutput(audioOutput);
+    player->setVideoOutput(videoWidget);
     audioOutput->setVolume(50);
     bool isPlaying = false;
+    qint64 currentPosition = 0;
 
-    QObject::connect(openFileButton, &QPushButton::clicked, [&filePath, player, &isPlaying, &playerWindow]()
+    QObject::connect(openFileButton, &QPushButton::clicked, [&filePath, player, &isPlaying, &playerWindow, songSlider, &currentPosition]()
     {
-       filePath = QFileDialog::getOpenFileName(nullptr, "Open file", "C:\\Qt\\Projects\\Less38_3", "mp3 files(*.mp3)");
+       filePath = QFileDialog::getOpenFileName(nullptr, "Open file", "C:\\Qt\\Projects\\Less38_3", "media files(*.mp3, *.mp4)");
        player->stop();
        isPlaying = false;
        playerWindow.setWindowTitle(filePath);
+       player->setSource(QUrl::fromLocalFile(filePath));
+       QObject::connect(player, &QMediaPlayer::durationChanged, [songSlider](qint64 duration)
+       {
+           songSlider->setMinimum(0);
+           songSlider->setMaximum(duration);
+       });
+       QObject::connect(player, &QMediaPlayer::positionChanged, [songSlider, &currentPosition](qint64 pos)
+       {
+           songSlider->setValue(pos);
+           currentPosition = pos;
+       });
     });
-    QObject::connect(playMediaButton, &QPushButton::clicked, [player, songSlider, &filePath, &isPlaying]()
+    QObject::connect(playMediaButton, &QPushButton::clicked, [player, &currentPosition, &isPlaying]()
     {
-        if(!isPlaying)
-        {
-            player->setSource(QUrl::fromLocalFile(filePath));
-            QObject::connect(player, &QMediaPlayer::durationChanged, [songSlider](qint64 duration)
-            {
-                songSlider->setMinimum(0);
-                songSlider->setMaximum(duration);
-            });
-            QObject::connect(player, &QMediaPlayer::positionChanged, [songSlider](qint64 pos)
-            {
-                songSlider->setValue(pos);
-            });
-            isPlaying = true;
-        }
+        isPlaying = true;
         player->play();
+        player->setPosition(currentPosition);
+    });
+    QObject::connect(songSlider, &QSlider::valueChanged, [player](int pos)
+    {
+        player->setPosition(pos);
+    });
+    QObject::connect(stopMediaButton, &QPushButton::clicked, [player, &currentPosition, songSlider, &isPlaying](){
+        player->stop();
+        isPlaying = false;
+        songSlider->setValue(0);
+        currentPosition = 0;
+    });
+    QObject::connect(pauseMediaButton, &QPushButton::clicked, [player, &isPlaying](){
+
+        player->pause();
+        isPlaying = false;
+
     });
     playerWindow.setWindowTitle("Multimedia player");
     playerWindow.show();
